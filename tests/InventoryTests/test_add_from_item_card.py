58299c00346c7a8c94e0d98f2e398e1f0f6ae478
Oladik Oladik <sniperhydra@gmail.com>
2026-04-07 14:09:39 +0300

    Feat: add test for adding items to cart from item's card

---
+++ tests/InventoryTests/test_add_from_item_card.py
@@ -0,0 +1,28 @@
+from pages.inventory_page import InventoryPage
+from pages.item_card_page import ItemCardPage
+
+import random
+
+def test_add_from_item_cart(logged_in_browser):
+    inventory_page = InventoryPage(logged_in_browser)
+
+    items = inventory_page.get_all_items()
+
+    item = random.choice(items)
+    name_inventory = item[0]
+    price_inventory = item[1]
+    inventory_page.click_item_title(name_inventory)
+
+    assert "inventory-item" in logged_in_browser.current_url
+
+    item_card_page = ItemCardPage(logged_in_browser)
+    name_card = item_card_page.get_name()
+    price_card = item_card_page.get_price()
+
+    assert name_inventory == name_card
+    assert price_inventory == price_card
+
+    item_card_page.add_to_cart()
+
+    assert item_card_page.get_text_from_cart_badge() == "1"
+    assert item_card_page.get_text_remove_button() == "addremove"

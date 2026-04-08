d0eb42b31c6f623b683eb57aebea8f397d3d3634
Oladik Oladik <sniperhydra@gmail.com>
2026-04-08 15:29:02 +0300

    Fix: fix and clean 'test_cart_badge_change'

--- tests/InventoryTests/test_cart_badge_change.py
+++ tests/InventoryTests/test_cart_badge_change.py
@@ -1,17 +1,13 @@
 import pytest
 
 from utility_funcs.load_data import load_data_json
-from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-@pytest.mark.parametrize("item_name", load_data_json("add_to_cart_data.json"))
-def test_add_to_cart(logged_in_browser, item_name):
-    actual_item_name = item_name[0]
-    invenortyPage = InventoryPage(logged_in_browser)
-    invenortyPage.add_item_to_cart_dynamic(actual_item_name)
+@pytest.mark.parametrize("test_data", load_data_json("add_to_cart_data.json"))
+def test_add_to_cart(logged_in_browser, test_data):
+    actual_item_name = test_data["item_name"]
+    inventoryPage = InventoryPage(logged_in_browser)
+    inventoryPage.add_item_to_cart_dynamic(actual_item_name)
+    inventoryPage.wait_for_cart_badge_to_update("1")
     
-    assert invenortyPage.get_text_from_cart_badge() == "1"
+    assert inventoryPage.get_text_from_cart_badge() == "1"

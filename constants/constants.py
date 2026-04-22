15e1de6baa5db6133b567437b1eba9ba193bf9b2
Oladik Oladik <sniperhydra@gmail.com>
2026-04-22 14:35:41 +0300

    Chore: add file for constants

---
+++ constants/constants.py
@@ -0,0 +1,8 @@
+EXPECTED_IMAGES_MAP = {
+    "Sauce Labs Backpack": "backpack",
+    "Sauce Labs Bike Light": "bike-light",
+    "Sauce Labs Bolt T-Shirt": "bolt-shirt",
+    "Sauce Labs Fleece Jacket": "pullover",
+    "Sauce Labs Onesie": "onesie",
+    "Test.allTheThings() T-Shirt (Red)": "red-tatt"
+}
